f9432de9da42993af9f698d53aa9fb22edbf2490
Meet Kumbhani <meetkumbhani512@gmail.com>
2025-08-18 12:08:20 +0530

    kem ho

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -32,6 +32,7 @@ const Homepaze = () => {
                     <button className='home-btn' onClick={() => {  navigate('/resume'); }}   style={{ cursor: 'pointer' }}>Build your free Resume</button>
                 </div>
             </form>
+            <h2>kem coh</h2>
             </div>
     </div>
   )
