f1cbf01cdf36ae04278f13a23b2ce686f695da0f
Meet Kumbhani <meetkumbhani512@gmail.com>
2025-11-10 14:57:41 +0530

    Merge branch 'dev10'

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://yourbackend.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://yourbackend.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
+
 
 
 export default API;
