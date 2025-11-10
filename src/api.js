1877c6d44648e8c774fe8d1d3d0132cede0038f6
Meet Kumbhani <meetkumbhani512@gmail.com>
2025-11-10 15:05:53 +0530

    Merge branch 'dev11'

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://yourbackend.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
   headers: { "Content-Type": "application/json" },
 });
 
